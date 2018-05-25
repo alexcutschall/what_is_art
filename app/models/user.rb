968b3af31d8eecd7a554e9d0ef5c4a6a29b33326
alexcutschall <alexander.cutschall@gmail.com>
2018-05-25 05:22:12 -0600

    adding to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :username, :password
   validates_presence_of :password, confirmation: true
   validates_uniqueness_of :username
-  has_attached_file :profile_pic, styles: { medium: "300x300>"}, default_url: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Ffc07.deviantart.net%2Ffs71%2Ff%2F2011%2F299%2Fc%2Fc%2Fprofile_picture_by_missing_man-d4e1qxe.jpg&f=1"
+  has_attached_file :profile_pic, styles: { medium: "300x300>"}
   validates_attachment_content_type :profile_pic, content_type: /\Aimage\/.*\z/
   has_secure_password
 end
