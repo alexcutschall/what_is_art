2233217ed3a991bc1e5da1c00dce4193cb75e6fc
alexcutschall <alexander.cutschall@gmail.com>
2018-05-29 12:01:30 -0600

    the link is now added in:

---
+++ spec/features/artwork/users_can_go_to_the_genes_page_for_an_artwork_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'as a user' do
+  context 'when they visit an artwork show page' do
+    feature 'when they click on the genes page' do
+      it 'shows them information concerning those genes' do
+        user = User.create(username: 'Wolverine', password: 'Logan', x_app_token: ENV['USER_XAPP_TOKEN'])
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        visit gallery_path
+        first('.image-link').click
+        click_on "Learn More About the Genes/Categories!"
+
+        expect(page).to have_content("Name")
+        expect(page).to have_content("Display Name")
+        expect(page).to have_content("Description")
+        expect(page).to have_content("Artworks")
+      end
+    end
+  end
+end
