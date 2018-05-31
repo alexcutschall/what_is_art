14f5457854cc72d01d3edf937f9eb672249ec222
alexcutschall <alexander.cutschall@gmail.com>
2018-05-31 15:48:10 -0600

    presenters are now in place for user artwork and artist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def present(model, presenter_class=nil)
+    klass = presenter_class || "#{model.class}Presenter".constantize
+    presenter = klass.new(model, self)
+    yield(presenter) if block_given?
+  end
 end
+
+# <%- @posts.each do |p| %>
+#   <% present(p) do |post| %>
+#     <tr>
+#       <td><%= post.id %></td>
+#       <td><%= post.title %></td>
+#       <td><%= post.body %></td>
+#       <td><%= post.publication_status %></td>
+#     </tr>
+#   <% end %>
+# <% end %>
