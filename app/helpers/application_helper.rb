053874a82e7d0c3529338931caad5d199b9b835a
alexcutschall <alexander.cutschall@gmail.com>
2018-05-31 16:39:36 -0600

    mores tyling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,3 @@ module ApplicationHelper
     yield(presenter) if block_given?
   end
 end
-
-# <%- @posts.each do |p| %>
-#   <% present(p) do |post| %>
-#     <tr>
-#       <td><%= post.id %></td>
-#       <td><%= post.title %></td>
-#       <td><%= post.body %></td>
-#       <td><%= post.publication_status %></td>
-#     </tr>
-#   <% end %>
-# <% end %>
