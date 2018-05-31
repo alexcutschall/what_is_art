29089d18641400fd33a82c4ff466a25a8e757786
alexcutschall <alexander.cutschall@gmail.com>
2018-05-31 15:50:57 -0600

    adding in genes

---
+++ app/presenters/gene_presenter.rb
@@ -0,0 +1,35 @@
+class GenePresenter < BasePresenter
+
+  def name
+    if @model.name?
+      @model.name
+    else
+      "Name Unknown"
+    end
+  end
+
+  def description
+    if @model.description?
+      @model.description
+    else
+      "Description Unknown"
+    end
+  end
+
+  def artworks
+    if @model.artworks?
+      @model.artworks
+    else
+      "Artworks Unknown"
+    end
+
+    def artists
+      if @model.artists?
+        @model.artists?
+      else
+        "Artists Unknown"
+      end
+    end
+  end
+
+end
