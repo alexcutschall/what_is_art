b6e5648d772d535393c03918a848d2a6bbd99d25
alexcutschall <alexander.cutschall@gmail.com>
2018-06-03 12:50:09 -0600

    added in parsing of the body to my jquery

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,11 +3,27 @@ $( document ).ready(() => {
   $('.artwork-button').on('click',
     function (event) {
       $('.artwork-heart').toggleClass('filled-heart');
-      const artworkId = $( "div.artwork-id" ).text();
-      const artworkTitle = $( "div.artwork-title" ).text();
-      const artworkTitle = $( "div.artwork-category" ).text();
-      const artworkTitle = $( "div.artwork-medium" ).text();
-      debugger;
-  });
+      const artworkId = $( "span.artwork-id" ).text();
+      const artworkTitle = $( "span.artwork-title" ).text();
+      const artworkCategory = $( "span.artwork-category" ).text();
+      const artworkMedium = $( "span.artwork-medium" ).text();
+      const artworkDate = $( "span.artwork-date" ).text();
+      const artworkSeries = $( "span.artwork-series" ).text();
+      const artworkLiterature = $( "span.artwork-literature" ).text();
+      const artworkCollectingInstitution = $( "span.artwork-collecting-institution" ).text();
+      // const artworkArtist = $( "span.artwork-artist" ).text();
+      // const artworkSimilarArtwork = $( "span.artwork-similar-artwork" ).text();
+      // const artworkGenes = $( "span.artwork-genes" ).text();
 
+      const body = {
+        artworkId: artworkId,
+        artworkTitle,
+        artworkCategory,
+        artworkMedium,
+        artworkDate,
+        artworkSeries,
+        artworkLiterature,
+        artworkCollectingInstitution,
+      };
+  });
 });
